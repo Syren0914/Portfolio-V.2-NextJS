91c172efdf3b1b14145ea6f8d8a35972ca9c63a2
Kira <78575847+Syren0914@users.noreply.github.com>
2024-05-15 13:29:15 -0400

    update

--- portfolio/app/(home)/page.tsx
+++ portfolio/app/(home)/page.tsx
@@ -11,19 +11,26 @@ import { AnimatedPinDemo } from './components/3D-pin'
 
 
 
+
 export default function page() {
   return (
+    
     <div className='min-h-screen bg-black overflow-hidden'>
-      <div className='dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative'>
-        <div className='max-w-6xl mx-auto p-5 '>
+      
+        <div className='dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative'>
+          <div className='max-w-6xl mx-auto p-5 '>
         
           <Navbar></Navbar>
           <HeroSection></HeroSection>
           
           
         </div>
+        
+        
         <div className='h-10 xl:h-32 bg-gradient-to-t from-black absolute -bottom-5 left-0 xl:bottom-0 w-full'></div>
+      
       </div>
+      
       
       
         <div className='max-w-7xl mx-auto p-5 mt-20 mb-42 '>
--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	metadataBase: new URL("https://localhost:3000"),
+	metadataBase: new URL("https://www.erdenebatbayar.com"),
 
 	title: "Erdene Portfolio",
 	authors: {
@@ -19,11 +19,11 @@ export const metadata: Metadata = {
 		description:
 			"Based in Virginia. I'm a Fullstack developer passionate about building a modern web application that user love.",
 		url: "https://localhost:3000",
-		siteName: "Erdene Portfolio",
+		siteName: "Erdene Batbayar - Portfolio 2024",
 		images: "/og.png",
 		type: "website",
 	},
-	keywords: ["syren0914", "Erdene", "portfolio"],
+	keywords: ["syren0914", "Erdene", "portfolio","Erdene Batbayar","Designer","Front End devoloper", "Fullstack Developer", "portfolio"],
 };
 
 
@@ -34,6 +34,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+		<head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icon.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icon.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icon.png" />
+      </head>
       <body className={spaceGrotesk.className}>
         <ThemeProvider
             attribute="class"
